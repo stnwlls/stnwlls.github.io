a9d70a0c33659987727af280107ff9d64fef89a1
Austin W. <AUS2172957@maricopa.edu>
2024-12-14 14:22:59 -0700

    update portolio projects-data.js and clean up folders/add projects

--- projects-data.js
+++ projects-data.js
@@ -1,13 +1,12 @@
 const projectsData = [
   {
-    title: "JavaScript Calculator with iPhone Design",
+    title: "JavaScript Calculator",
     description: "Developed a functional calculator application using HTML, CSS, and JavaScript. The interface is designed to replicate the aesthetic of an iPhone calculator, featuring responsive buttons, a sleek layout, and accurate calculations. Key functionalities include basic arithmetic operations, toggle for negative/positive numbers, and error handling for invalid inputs.",
     month: "November",
     year: 2024,
     links: {
-      imagelink: "/projects/javascriptCalculator.png", // Replace with a screenshot path
-      github: "", // Add a GitHub link here
-      liveDemo: "", // Add a live demo link if available
+      github: "https://github.com/stnwlls/JSCalculator", // Add a GitHub link here
+      live: "/projects/jscalculator/calculator.html"
     },
     hashtags: [
       "#html",
@@ -17,199 +16,185 @@ const projectsData = [
     image: "https://placehold.co/150x250",
   },
   {
-    title: "Bootstrap Final Project: Kelp Shake Website",
-    description: "Created a fully responsive website from scratch using Bootstrap 5 for a fictional brand, Kelp Shake, inspired by SpongeBob. The project includes dynamic components like a carousel, dropdown menus, and custom-designed cards. Leveraging Bootstrap utilities and custom CSS, the website showcases a modern, user-friendly design. Features include a visually appealing hero section, a detailed navigation bar, CTA buttons, and responsive layouts that adapt seamlessly across devices.",
+    title: "Bootstrap Project: Kelp Shake Webpage",
+    description: "Created a fully responsive webpage from scratch using Bootstrap 5 for a fictional brand, Kelp Shake, inspired by SpongeBob. The project includes dynamic components like a carousel, dropdown menus, and custom-designed cards. Leveraging Bootstrap utilities and custom CSS, the webpage showcases a modern, user-friendly design.",
     month: "November",
     year: 2024,
     links: {
-      github: ".", // Placeholder for GitHub repository link
-      live: ".", // Placeholder for live project demo
+      github: "https://github.com/stnwlls/kelpshake",
+      live: "/projects/kelpshake/index.html"
     },
     hashtags: [
       "#html",
       "#css",
       "#bootstrap",
-      "#rwd",
-      "#webdevelopment"
+      "#rwd"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
+  // {
+  //   title: "Bootstrap Responsive Web Page Design",
+  //   description: "Enhanced a pre-built HTML template by integrating Bootstrap for responsive design and modern UI components. Key features include a dynamic navigation bar, an engaging carousel, and a grid-based layout for showcasing travel options. The project demonstrates proficiency in implementing Bootstrap utilities and components, including responsive breakpoints, text styling, and carousel controls, creating a professional and interactive user experience.",
+  //   month: "November",
+  //   year: 2024,
+  //   links: {
+  //     github: "a", // Placeholder for GitHub repository link
+  //     live: "a", // Placeholder for live project demo
+  //   },
+  //   hashtags: [
+  //     "#html",
+  //     "#css",
+  //     "#bootstrap",
+  //     "#rwd"
+  //   ],
+  //   image: "https://placehold.co/150x250",
+  // },
   {
-    title: "Bootstrap Responsive Web Page Design",
-    description: "Enhanced a pre-built HTML template by integrating Bootstrap for responsive design and modern UI components. Key features include a dynamic navigation bar, an engaging carousel, and a grid-based layout for showcasing travel options. The project demonstrates proficiency in implementing Bootstrap utilities and components, including responsive breakpoints, text styling, and carousel controls, creating a professional and interactive user experience.",
-    month: "November",
-    year: 2024,
-    links: {
-      github: "a", // Placeholder for GitHub repository link
-      live: "a", // Placeholder for live project demo
-    },
-    hashtags: [
-      "#html",
-      "#css",
-      "#bootstrap",
-      "#rwd",
-      "#webdevelopment"
-    ],
-    image: "https://placehold.co/150x250",
-  },
-  {
-    title: "Content Audit and Redesign of Memorial Health Website",
-    description: "Conducted a content audit and full redesign of the Memorial Health Bariatric Surgery page using Figma. The redesign focused on reorganizing content for improved user experience and readability. Key changes included consolidating large blocks of text into smaller, scannable sections with clear headings, integrating more engaging visuals, and adding prominent call-to-action (CTA) buttons to enhance user navigation. The redesigned page emphasizes accessibility, content hierarchy, and aesthetic appeal, creating a more user-friendly experience.",
+    title: "Content Audit and Redesign in Figma",
+    description: "Conducted a content audit and full redesign of the Memorial Health Bariatric Surgery page using Figma. The redesign focused on reorganizing content for improved user experience and readability. Key changes included consolidating large blocks of text into smaller, scannable sections with clear headings, and integrating more engaging visuals.",
     month: "October",
     year: 2024,
     links: {
       imagelink: "/projects/MemorialHealthRedesign.png",
-      figma: "https://www.figma.com/design/Pn2gWzyC0xNgUyZNxTUiw3/MemorialHealth.com-Redesign?node-id=0-1&t=51DfiOa4aWh5j1YC-1",
+      figma: "https://www.figma.com/design/Pn2gWzyC0xNgUyZNxTUiw3/MemorialHealth.com-Redesign?node-id=0-1&t=51DfiOa4aWh5j1YC-1"
     },
     hashtags: [
       "#figma",
       "#contentaudit",
       "#uxdesign",
-      "#uidesign",
-      "#webdevelopment"
+      "#uidesign"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
   {
-    title: "Usability Testing for Sutra Studios Website",
-    description: "Conducted a usability test for the Sutra Studios website, focusing on user interaction, navigation, and purchase flow. The test was performed with a participant to identify pain points and gauge the clarity of the website's purpose. This project involved recording user feedback, analyzing website usability, and providing actionable recommendations for improvement. Key insights included confusion about the site's navigation, challenges with locating the class booking process, and suggestions for clearer visual hierarchy.",
+    title: "Usability Test",
+    description: "Conducted a usability test for the Sutra Studios website, focusing on user interaction, navigation, and purchase flow. The test was performed with a participant to identify pain points and gauge the clarity of the website's purpose. This project involved recording user feedback, analyzing website usability, and providing actionable recommendations for improvement.",
     month: "October",
     year: 2024,
     links: {
-      youtube: "https://www.youtube.com/watch?v=Wpamxk-HVfU",
+      youtube: "https://www.youtube.com/watch?v=Wpamxk-HVfU"
     },
     hashtags: [
       "#usabilitytesting",
-      "#uxresearch",
-      "#webusability",
-      "#webdevelopment"
+      "#uxresearch"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
   {
     title: "A/B Testing Mobile App Prototype in Figma",
-    description: "Created two distinct prototypes for the TourView mobile app to conduct an A/B test, focusing on user interface (UI) and user experience (UX) design. Leveraged Figma and adhered to Apple's Human Interface Guidelines to ensure the design stayed true to iOS standards. The prototypes include a text-based search interface (Prototype A) and a map-based selection interface (Prototype B). Designed specifically for iPhone, this project demonstrates a commitment to detailed, platform-optimized mobile app design and usability testing.",
+    description: "Created two distinct prototypes for the fictional TourView mobile app to conduct an A/B test, focusing on user interface (UI) and user experience (UX) design. Leveraged Figma and adhered to Apple's Human Interface Guidelines to ensure the design stayed true to iOS standards. The prototypes include a text-based search interface (Prototype A) and a map-based selection interface (Prototype B).",
     month: "October",
     year: 2024,
     links: {
       imagelink: "/projects/tourviewAppABTesting.png",
-      figma: "https://www.figma.com/design/hdSsIvCViqqQwjY8E6VIbR/tourviewAppABtesting?node-id=0-1&t=UG0HAuUumVha94cf-1",
+      figma: "https://www.figma.com/design/hdSsIvCViqqQwjY8E6VIbR/tourviewAppABtesting?node-id=0-1&t=UG0HAuUumVha94cf-1"
     },
     hashtags: [
       "#figma",
       "#abtesting",
       "#mobileappdesign",
-      "#iosdesign",
       "#uxdesign",
       "#uidesign"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
   {
     title: "Kelp Shake Prototype in Adobe XD",
-    description: "Decided to have fun with this project by creating a website to promote the Kelp Shake, inspired by SpongeBob. Developed a high-fidelity prototype using Adobe XD that replaced all coffee-related elements with custom branding and data for Kelp Shake. The project includes a fully designed layout with a responsive navigation bar, interactive elements, and a footer featuring essential links. This prototype demonstrates expertise in user experience, visual design, and cohesive branding, taking the project above and beyond the original requirements.",
+    description: "Decided to have fun with this project by creating a webpage to promote the Kelp Shake, inspired by SpongeBob. Developed a high-fidelity prototype using Adobe XD. The project includes a fully designed layout with a responsive navigation bar, interactive elements, and a footer featuring essential links. This prototype demonstrates expertise in user experience, visual design, and cohesive branding.",
     month: "October",
     year: 2024,
     links: {
       imagelink: "/projects/kelpshakePrototype.png",
-      adobeXD: "https://xd.adobe.com/view/275f8527-0104-4293-8cd9-244fd51ab0ee-720c/?fullscreen",
+      adobeXD: "https://xd.adobe.com/view/275f8527-0104-4293-8cd9-244fd51ab0ee-720c/?fullscreen"
     },
     hashtags: [
       "#adobexd",
       "#uxdesign",
       "#uidesign",
-      "#prototyping",
-      "#webdevelopment"
+      "#prototyping"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
   {
-    title: "Website Wireframe Design in Figma",
-    description: "Developed a detailed wireframe for the home page of orbitmedia.com using FigJam. The project involved recreating the website's header, footer, and five key sections of the page based on the desktop view. This wireframe showcases expertise in visual hierarchy, content arrangement, and design tools to plan web layouts effectively. Placeholders for text and images were used to simulate content and structure, adhering to the principles of wireframing.",
+    title: "Wireframe Design in Figma",
+    description: "Developed a wireframe for the home page of orbitmedia.com using Figma. The project involved recreating the website's header, footer, and five key sections of the page based on the desktop view. This wireframe showcases expertise in visual hierarchy, content arrangement, and design tools to plan web layouts effectively. Placeholders for text and images were used to simulate content and structure, adhering to the principles of wireframing.",
     month: "October",
     year: 2024,
     links: {
       imagelink: "/projects/orbitmediaWireframe.png",
-      figma: "https://www.figma.com/design/WPT1PFGcqG4dJHvqBLySnH/orbitmedia.com-Wireframe?node-id=303-5555&t=KxIYrLS2ooxaxGqC-1",
+      figma: "https://www.figma.com/design/WPT1PFGcqG4dJHvqBLySnH/orbitmedia.com-Wireframe?node-id=303-5555&t=KxIYrLS2ooxaxGqC-1"
     },
     hashtags: [
       "#figma",
       "#wireframe",
-      "#uxdesign",
-      "#webdevelopment"
+      "#uxdesign"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },
   {
-    title: "Website Site Map Creation in FigJam",
+    title: "Site Map Creation in FigJam",
     description: "Created an interactive and detailed site map for the Phoenix Rising FC website using FigJam. This project focused on replicating the navigation structure of the site, including all primary navigation links and sub-menu links. It demonstrates proficiency in analyzing website architecture and using collaborative design tools like FigJam for visual organization.",
     month: "September",
     year: 2024,
     links: {
       imagelink: "/projects/phxrisingSiteMap.jpg",
-      figma: "https://www.figma.com/board/a8VpQT39x7YDHnHSB3WjOk/phxrisingfc.com-Site-Map?node-id=0-1&t=ejO5Hl07MSwEh5to-1",
+      figma: "https://www.figma.com/board/a8VpQT39x7YDHnHSB3WjOk/phxrisingfc.com-Site-Map?node-id=0-1&t=ejO5Hl07MSwEh5to-1"
     },
     hashtags: [
       "#figma",
       "#sitemap",
-      "#uxdesign",
-      "#webdevelopment"
+      "#uxdesign"
     ],
-    image: "https://placehold.co/150x250",
+    image: "https://placehold.co/150x250"
   },  
   {
-    title: "Frontend Development Final Project",
-    description: "Completed a three-page website demonstrating proficiency in HTML, CSS, JavaScript, and Bootstrap. The project emphasized CSS Grid for layout, responsive web design (RWD) principles, and reusable external stylesheets. Key features include a responsive Bootstrap navigation bar, functional form, embedded video, and styled table.",
+    title: "Three Page Website with CSS Grid",
+    description: "Completed a three-page website demonstrating proficiency in HTML and CSS. The project emphasized CSS Grid for layout, responsive web design (RWD) principles, and reusable external stylesheets. Key features include a responsive Bootstrap navigation bar, functional form, embedded video, and styled table.",
     month: "July",
     year: 2024,
     links: {
-      github: "www.github.com", // Placeholder for GitHub link
-      live: "www.live.com", // Placeholder for live demo link
+      github: "https://github.com/stnwlls/threepagesite",
+      live: "/projects/threepagesite/index.html"
     },
-    hashtags: ["#html", "#css", "#javascript", "#cssgrid", "#bootstrap", "#rwd", "#webdevelopment"],
-    image: "https://placehold.co/150x250",
-    folder: "texas site"
+    hashtags: [
+      "#html", 
+      "#css", 
+      "#bootstrap", 
+      "#rwd"
+    ],
+    image: "https://placehold.co/150x250"
   },
   {
-    title: "Advanced HTML & CSS Techniques",
-    description: "Showcased proficiency in advanced HTML and CSS skills, focusing on responsive web design (RWD) principles. The project featured side-by-side layouts, media queries for adaptability, and custom typography integration. Special emphasis was placed on user experience and visual design with pull-quotes, responsive images, and embedded media.",
-    month: "July",
+    title: "Showcasing CSS Techniques",
+    description: "Showcased proficiency in HTML and CSS skills. The project featured side-by-side layouts to comapre and media queries for adaptability. Special emphasis was placed on user experience and visual design with pull-quotes, responsive images, and embedded media.",
+    month: "June",
     year: 2024,
     links: {
-      live: "www.live.com", // Placeholder for live demo link
+      github: "https://github.com/stnwlls/csstechniques",
+      live: "/projects/csstechniques/historyOfApple.html"
     },
-    hashtags: ["#html", "#css", "#rwd", "#webdevelopment"],
-    image: "https://placehold.co/150x250",
-    folder: "history of apple"
+    hashtags: [
+      "#html", 
+      "#css"
+    ],
+    image: "https://placehold.co/150x250"
   },
   {
-    title: "Dynamic Table with External Data Integration",
+    title: "Webpage with External Data Integration",
     description: "Created a responsive webpage featuring a dynamically generated table using data fetched from JSON and XML files. Implemented CSS Grid for layout, responsive design techniques, and structured data handling to ensure seamless presentation.",
     month: "June",
     year: 2024,
     links: {
-      github: "www.github.com", // Placeholder for GitHub link
-      live: "www.github.com", // Placeholder for live demo link
+      github: "https://github.com/stnwlls/dataintegration",
+      live: "/projects/dataintegration/index.html"
     },
-    hashtags: ["#html", "#css", "#javascript", "#cssgrid", "#json", "#xml", "#webdevelopment"],
-    image: "https://placehold.co/150x250",
-    folder: "apple history"
-  },
-  {
-    title: "Responsive Web Design Assignment",
-    description: "Mastered responsive web design (RWD) principles by creating layouts for small, medium, and large screens. Leveraged media queries, fluid layouts, and advanced CSS techniques like calculating total widths and using relative measurements. Developed a mobile-first approach with responsive images and adaptive typography.",
-    month: "June",
-    year: 2024,
-    links: {
-      github: "www.github.com", // Placeholder for GitHub link
-      live: "www.live.com", // Placeholder for live demo link
-      youtube: "www.youtube.com", // Placeholder for YouTube link
-      figma: "www.figma.com", // Placeholder for Figma link
-      adobeXD: "www.adobexd.com", // Placeholder for Adobe XD link
-    },
-    hashtags: ["#html", "#css", "#rwd", "#webdevelopment"],
-    image: "https://placehold.co/150x250",
-    folder: "designs"
+    hashtags: [
+      "#html", 
+      "#css",
+      "#json", 
+      "#xml"
+    ],
+    image: "https://placehold.co/150x250"
   }
 ];
 
